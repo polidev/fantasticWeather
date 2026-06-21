76b786b523b0d603c204ae22fe6bef3d5ab9efe3
Jesus Politron <jesuspolitronrangel@gmail.com>
2026-06-21 14:55:30 -0600

    add: weather card template

---
+++ src/components/searchBar/searchBar.jsx
@@ -0,0 +1,5 @@
+import "./searchBar.css";
+
+export default function SearchBar() {
+  return <input type="text" placeholder="Search..." className="search-bar" />;
+}
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,18 @@
+import SearchBar from "../components/searchBar/searchBar.jsx";
+import WeatherCard from "../components/weatherCard/weatherCard.jsx";
 import "./home.css";
 
 export default function Home() {
   return (
     <main className="home">
-      <h1 className="text-4xl font-bold mb-4">Welcome to Fantastic Weather!</h1>
-      <p className="">
-        Your go-to app for accurate and up-to-date weather forecasts.
-      </p>
+      <SearchBar />
+      <WeatherCard
+        weatherData={{
+          city: "New York",
+          temperature: 25,
+          description: "Sunny",
+        }}
+      />
     </main>
   );
 }
