ddb6ec1b783e5ebbcd2989df3804881e5d41c1fd
Jesus Politron <jesuspolitronrangel@gmail.com>
2026-06-21 14:25:08 -0600

    add: api key file and miscelaneous config

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router";
 
+const Home = lazy(() => import("./pages/home.jsx"));
+
 function App() {
   return (
     <>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path="/" element={<div>Home</div>} />
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<div>About</div>} />
         </Routes>
       </Suspense>
---
+++ src/pages/home.jsx
@@ -0,0 +1,12 @@
+import "./home.css";
+
+export default function Home() {
+  return (
+    <main className="home">
+      <h1 className="text-4xl font-bold mb-4">Welcome to Fantastic Weather!</h1>
+      <p className="">
+        Your go-to app for accurate and up-to-date weather forecasts.
+      </p>
+    </main>
+  );
+}
