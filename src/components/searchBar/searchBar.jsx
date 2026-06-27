c235c5ddd22653b8586b11f6e19df5932e72ae23
Jesus Politron <jesuspolitronrangel@gmail.com>
2026-06-27 07:51:18 -0600

    add: city api call request

--- src/components/searchBar/searchBar.jsx
+++ src/components/searchBar/searchBar.jsx
@@ -1,5 +1,33 @@
+import { useFetchWeatherData } from "../../api/weather.js";
 import "./searchBar.css";
 
 export default function SearchBar() {
-  return <input type="text" placeholder="Search..." className="search-bar" />;
+  const handleSearch = (event) => {
+    event.preventDefault();
+    const searchInput = document.getElementById("search-input");
+    const city = searchInput.value.trim().toLowerCase();
+    console.log(city);
+    // useFetchWeatherData(city);
+    searchInput.value = "";
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      handleSearch(event);
+    }
+  };
+
+  return (
+    <form id="search-form" action="" onSubmit={handleSearch}>
+      <input
+        id="search-input"
+        type="text"
+        placeholder="e.g. Guadalajara"
+        className="search-bar"
+        onKeyPress={handleKeyPress}
+        required
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
 }
